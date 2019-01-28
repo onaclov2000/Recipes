6664fbc3a9784f220e46d5d270f4b1328d8e98bf
Tyson Bailey <onaclov2000@gmail.com>
2019-01-28 07:33:47 -0700

    Initial attempt at Cookie K Means Clustering

--- analysis/cookie_map.py
+++ analysis/cookie_map.py
@@ -1,15 +1,15 @@
 import pandas as pd
 import numpy as np
-
+from sklearn.cluster import KMeans
 df = pd.read_csv('cookies.csv')
 ingredients_columns = set(df.columns) - set(['Consistency (Soft, Chewy, Etc)', 'Note', 'From', 'Have I made', 
                                              'Dough Temp/Consistency', 'Shortening Flavor',"Water (Hot,Cold)", 
-                                             'Butter Prep', 'Butter Type', "salt (table, fine, coarse)"])
+                                             'Butter Prep', 'Butter Type', "salt (table, fine, coarse)", "Altitude"])
 print (ingredients_columns)
 print (df[ingredients_columns])
 for i in ingredients_columns:
-   print (i)
-   print (np.unique(df[i].values))
+   print (i, np.unique(df[i].values))
 
-
-print (df["Baking Powder"])
+kmeans = KMeans(n_clusters=4, random_state=0).fit(df[ingredients_columns].values)
+kmeans.labels_
+print (kmeans.predict(df[ingredients_columns].values))
