2aae58420fbca49c64b3a8197b0eed3f71e3a69f
Tyson Bailey <onaclov2000@gmail.com>
2019-01-28 19:04:07 -0700

    Updates to add .json option, so I can add new recipes easier

--- analysis/cookie_map.py
+++ analysis/cookie_map.py
@@ -1,7 +1,18 @@
+import matplotlib
+matplotlib.use('Agg')
+import json
 import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 df = pd.read_csv('cookies.csv')
+
+#df.to_json("cookies.json", orient='index')
+df_json_pretty = json.dumps(json.loads(df.to_json(orient='records')), indent=2, sort_keys=True)
+f = open('cookies.json', 'w')
+f.write(df_json_pretty)
+f.close()
+exit()
 ingredients_columns = set(df.columns) - set(['Consistency (Soft, Chewy, Etc)', 'Note', 'From', 'Have I made', 
                                              'Dough Temp/Consistency', 'Shortening Flavor',"Water (Hot,Cold)", 
                                              'Butter Prep', 'Butter Type', "salt (table, fine, coarse)", "Altitude"])
@@ -13,3 +24,10 @@ for i in ingredients_columns:
 kmeans = KMeans(n_clusters=4, random_state=0).fit(df[ingredients_columns].values)
 kmeans.labels_
 print (kmeans.predict(df[ingredients_columns].values))
+centers = kmeans.cluster_centers_
+print (centers)
+for i in range(len(ingredients_columns)-1):
+    plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5);
+    plt.scatter(df[ingredients_columns].values[:,i], df[ingredients_columns].values[:,i+1], cmap='viridis')
+    plt.savefig('centers_' + str(i) + '.png')
+    plt.clf()
